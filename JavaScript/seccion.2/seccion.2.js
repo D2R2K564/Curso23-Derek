09f6e40f2987492b050850946db4ad202b8182ca
D2R2K564 <derekamir31@gmail.com>
2025-08-20 20:33:05 -0400

    js,2

--- JavaScript/seccion.2/seccion.2.js
+++ JavaScript/seccion.2/seccion.2.js
@@ -384,3 +384,19 @@ function verificar_palindromo(texto) {
 
 verificar_palindromo("A ti no, bonita.");
 
+//Metodos de arrays
+/*
+  Los métodos de arrays son funciones que se pueden aplicar a arrays para realizar diversas operaciones.
+  Algunos de los métodos más comunes son:
+  - `push()`: Agrega uno o más elementos al final del array.
+  - `pop()`: Elimina el último elemento del array y lo devuelve.
+  - `shift()`: Elimina el primer elemento del array y lo devuelve.
+  - `unshift()`: Agrega uno o más elementos al inicio del array.
+  - `splice()`: Cambia el contenido de un array eliminando o reemplazando elementos existentes y/o agregando nuevos elementos en su lugar.
+  - `slice()`: Devuelve una copia superficial de una porción del array dentro de un nuevo array.
+  - `forEach()`: Ejecuta una función proporcionada una vez por cada elemento del array.
+  - `map()`: Crea un nuevo array con los resultados de la llamada a la función proporcionada en cada elemento del array.
+  - `filter()`: Crea un nuevo array con todos los elementos que cumplan la condición implementada por la función proporcionada.
+  - `find()`: Devuelve el primer elemento del array que cumple con la condición proporcionada en la función.
+  - `includes()`: Verifica si un elemento está presente en el array.
+*/
